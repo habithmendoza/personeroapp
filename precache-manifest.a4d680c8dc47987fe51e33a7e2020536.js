c48a5bdc0b5a8d1889fcc81160841917a536bd65
habimendoza <habimendoza@hotmail.com>
2019-11-20 19:48:49 -0500

    Updates

---
+++ precache-manifest.a4d680c8dc47987fe51e33a7e2020536.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2add09268d5573278c0c32a2bca82428",
+    "url": "/personeroapp/index.html"
+  },
+  {
+    "revision": "2f02f077ced1e60e2af2",
+    "url": "/personeroapp/static/css/main.cc3e2925.chunk.css"
+  },
+  {
+    "revision": "9f86be71cb9583293247",
+    "url": "/personeroapp/static/js/2.2e7c160b.chunk.js"
+  },
+  {
+    "revision": "2f02f077ced1e60e2af2",
+    "url": "/personeroapp/static/js/main.8993e08d.chunk.js"
+  },
+  {
+    "revision": "e9f475044ae507889360",
+    "url": "/personeroapp/static/js/runtime-main.165c5f59.js"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/personeroapp/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/personeroapp/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/personeroapp/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/personeroapp/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c4a04ae8427764e525419cf63ee73f53",
+    "url": "/personeroapp/static/media/close.c4a04ae8.svg"
+  },
+  {
+    "revision": "bb36327e6625a8ab2b11005d72c6f763",
+    "url": "/personeroapp/static/media/img1.bb36327e.png"
+  },
+  {
+    "revision": "cb106d106a72dd723b42da6f39c56816",
+    "url": "/personeroapp/static/media/img2.cb106d10.png"
+  },
+  {
+    "revision": "cdaa61dca4cd00cb350a9c354a1937b6",
+    "url": "/personeroapp/static/media/img3.cdaa61dc.png"
+  },
+  {
+    "revision": "f0877bf6ed0f6c36f1158d291ade7570",
+    "url": "/personeroapp/static/media/thank.f0877bf6.png"
+  }
+]);
